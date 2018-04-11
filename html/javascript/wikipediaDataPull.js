6687ee322527f94923f1616a4137c75923e3201c
marmpa <marmpa@gmail.com>
2018-04-11 13:46:39 +0300

    fixed file problem

--- html/javascript/wikipediaDataPull.js
+++ html/javascript/wikipediaDataPull.js
@@ -1,6 +1,6 @@
 $(document).ready(function ()
 {
-$.getJSON('http://en.wikipedia.org/w/api.php?action=parse&page=google&prop=text&format=json&callback=?',function(json))
+$.getJSON('http://en.wikipedia.org/w/api.php?action=parse&page=google&prop=text&format=json&callback=?',function(json)
 	{
 		$('#wikiInfo').html(json.parse.text[*]);
 		$('#wikiInfo').find("a:not(.references a)").attr("href", function(){return "http://www.wikipedia.org" + $(this).attr("href");});
