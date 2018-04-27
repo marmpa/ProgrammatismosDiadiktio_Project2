9b619fde7ef78160d0be3982b4f0ae1edc56f865
marmpa <marmpa@gmail.com>
2018-04-27 21:36:22 +0300

    Kinda fixed wikipedia pulling thing and now testing

--- html/javascript/wikipediaDataPull.js
+++ html/javascript/wikipediaDataPull.js
@@ -1,13 +1,29 @@
-let site = 'http://en.wikipedia.org/w/api.php?action=query&prop=revisions&rvprop=content&format=json&titles=google&rvsection=0';
+var site = 'http://en.wikipedia.org/w/api.php?action=query&prop=revisions&rvprop=content&format=json&titles=google&rvsection=0';
 console.log(site);
 
 
 
 $(document).ready(function ()
 {
-$.getJSON('http://en.wikipedia.org/w/api.php?action=parse&page=Google&prop=text&format=json&callback=?',function(json)
+
+	var searchTerm = "Greece";
+	var url="http://en.wikipedia.org/w/api.php?action=parse&format=json&page=" + searchTerm+"&redirects&prop=text&callback=?";
+	var site = 'http://en.wikipedia.org/w/api.php?action=query&prop=revisions&rvprop=content&format=json&titles='+searchTerm+'&rvsection=0';
+
+$.getJSON(site,function(json)
 	{
-		var a = json.parse.text['*'];
+		wikiHTML = json.parse.text["*"];
+
+		var pattern = /Gini\s=\s\d+/i;
+
+		console.log(JSON.stringify(wikiHTML));
+		$("#wikiInfo").append(wikiHTML.match(pattern));
+
+		//$wikiDOM = $("<document>"+wikiHTML+"</document>");
+		//$("#wikiInfo").append($wikiDOM.find('.infobox').html());
+		//console.log("Hello    "+document.getElementById('wikiInfo').innerHTML);
+
+		//var a = json.parse.text['*'];
 		//$('#wikiInfo').html(json.parse.text['*']);
 		//$('#wikiInfo').find("a:not(.references a)").attr("href", function(){return "http://www.wikipedia.org" + $(this).attr("href");});
 		//$('#wikiInfo').find("a").attr("target", "_blank");
@@ -18,6 +34,8 @@ $.getJSON('http://en.wikipedia.org/w/api.php?action=parse&page=Google&prop=text&
 
 //http://en.wikipedia.org/w/api.php?action=query&prop=revisions&rvprop=content&format=json&titles=google&rvsection=0
 // original working: http://en.wikipedia.org/w/api.php?action=parse&page=google&prop=text&format=json&callback=?
+
+//kinda works 8:32
 
 /*$.getJSON('http://en.wikipedia.org/w/api.php?action=parse&page=google&prop=text&format=json&callback=?',function(json)
 					{
